61109457bdb265aa4b4255797420bc43e00cd21f
João Henrique <joaoenrique.san@gmail.com>
2022-01-06 17:47:39 -0300

    feat: finish section 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -28,8 +29,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js \n', expense);
+  };
+
   return (
     <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState('2019');
+
+  const setFilterYearHandler = (year) => {
+    selectedYear(year);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter
+        selected={selectedYear}
+        onChangeYear={setFilterYearHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
